d1f7e2dbad5d33e96b5d897c90a99321055bc11b
Marcos Fagundes <marcos733tuto@gmail.com>
2025-02-11 12:38:39 -0300

    Update page.tsx

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -3,7 +3,7 @@ import {Greeting} from "@/components/Greeting"
 
 const Home = () => {
   return (
-    <div className="w-screen h-screen p-10 flex flex-col items-center text-4xl font-bold text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+    <div className="w-screen h-screen p-10 flex flex-col items-center text-4xl font-bold text-white bg-gradient-to-r from-sky-950 to-gray-950">
 
       <Greeting></Greeting>
       <Clock></Clock>
